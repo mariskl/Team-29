3430bb1c9204e26a5f6021014f2052a96799d4f9
DESKTOP-QTUASIK\iljaj <svetlana.jamkina@gmail.com>
2024-09-09 09:31:33 +0300

    commit

--- java/Datorium-API/src/main/java/com/datorium/Datorium/API/Repo/UserRepo.java
+++ java/Datorium-API/src/main/java/com/datorium/Datorium/API/Repo/UserRepo.java
@@ -3,6 +3,8 @@ package com.datorium.Datorium.API.Repo;
 import com.datorium.Datorium.API.DTO.User;
 import org.springframework.stereotype.Repository;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,21 +13,26 @@ public class UserRepo {
 
     private final List<User> users = new ArrayList<>(); //Mocked db
 
-    public int add(User user) {
-        users.add(user);
-        return users.size();
+    public int add(User user){
+        String url = "jdbc:sqlite:my.db";
+        try (var conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                var statement = conn.createStatement();
+                statement.execute("INSERT INTO people (name) VALUES ('" + user.name + "')");
+                //INSERT INTO people (name) VALUES ('');DROP TABLE people;--')
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return 1;
     }
-
-    //public List<User> findAll() {
-      //  return new ArrayList<>(users);
-    //}
 
     public List<User> get(){
-
         return users;
     }
-    public User update(int userIndex, User updateUserDTO){
-        var user = users.get(userIndex);
+
+    public User update(int numberOfChristmasPresents, User updateUserDTO){
+        var user = users.get(numberOfChristmasPresents);
         user.name = updateUserDTO.name;
         return user;
     }
