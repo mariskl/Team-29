1f6540e3161c3483ce4d26bf1d686a87c6ade129
DESKTOP-QTUASIK\iljaj <svetlana.jamkina@gmail.com>
2024-09-09 12:25:30 +0300

    commit

--- java/Datorium-API/src/main/java/com/datorium/Datorium/API/Repo/UserRepo.java
+++ java/Datorium-API/src/main/java/com/datorium/Datorium/API/Repo/UserRepo.java
@@ -27,9 +27,28 @@ public class UserRepo {
         return 1;
     }
 
-    public List<User> get(){
-        return users;
+    public List<User> get() {
+        String url = "jdbc:sqlite:my.db"; //LOCATION of database
+        var resultList = new ArrayList<User>(); //prepare a box
+        try (var conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                var statement = conn.createStatement();//Create action what to do
+                var result = statement.executeQuery("SELECT name FROM people");
+                //DIFFERENT BOX, but more abstract
+
+                while(result.next()){ //going through abstract box
+                    var user =new User();//Create new user
+                    var name = result.getString("name");//assign name to the new user
+                    user.setName(name);
+                    resultList.add(user);// Add user to the box
+                } //While loop stops when there is no nmext element
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return resultList; //Return all the users assigned to the box
     }
+
 
     public User update(int numberOfChristmasPresents, User updateUserDTO){
         var user = users.get(numberOfChristmasPresents);
